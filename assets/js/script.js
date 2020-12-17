afe6df79c5fe5e57bd83b45eb5cd4b7f3b5f3d7c
Duc Dinh <holahu9@gmail.com>
2020-12-17 15:05:34 -0500

    ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,272 @@
 alert("files connected");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Create ajax calls to retrieve movie info
+
+var movieURL = "https://...";
+$.ajax({
+    url: URL,
+    method: "GET",
+}).then(function(response){
+
+    if (response.error === " NOT FOUND"){
+            //
+    }
+    else {
+    
+
+        //movie title
+        var mTitle = response.title;
+
+        //plot
+        var mplot = response.plot;
+
+
+        //rating
+        var mRating = response.rating;
+        
+
+
+        //runningTime
+        var mTime = response.runningTime;
+
+        //genre
+        var mgenre = response.genre;
+
+
+
+        //releaseDate
+        var mDate = response.releaseDate;
+
+
+
+    }
+
+}
+)
+
+
+
+//Create ajax calls to retrieve book info
+
+$.ajax({
+    url: bookURL,
+    method: "GET",
+  }).then(function (response1) {
+
+    if (response1.items === 0){
+
+
+
+    }
+
+
+
+
+    
+
+    else {
+
+
+
+
+    }
+
+
+  })
