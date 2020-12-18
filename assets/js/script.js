017bea2f0542b812919d5ce0da2a19f6ed1cf08f
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2020-12-17 21:17:13 -0500

    cleanup ajax calls, css file name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,275 +1,3 @@
-alert("files connected");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Create ajax calls to retrieve movie info
-
-var movieURL = "https://...";
-$.ajax({
-    url: URL,
-    method: "GET",
-}).then(function(response){
-
-    if (response.error === " NOT FOUND"){
-            //
-    }
-    else {
-    
-
-        //movie title
-        var mTitle = response.title;
-
-        //plot
-        var mplot = response.plot;
-
-
-        //rating
-        var mRating = response.rating;
-        
-
-
-        //runningTime
-        var mTime = response.runningTime;
-
-        //genre
-        var mgenre = response.genre;
-
-
-
-        //releaseDate
-        var mDate = response.releaseDate;
-
-
-
-    }
-
-}
-)
-
-
-
-//Create ajax calls to retrieve book info
-
-$.ajax({
-    url: bookURL,
-    method: "GET",
-  }).then(function (response1) {
-
-    if (response1.items === 0){
-
-
-
-    }
-
-
-
-
-    
-
-    else {
-
-
-
-
-    }
-
-
-  })
 var searchInfo = {
     author: {
         firstName: "Dan",
@@ -428,3 +156,127 @@ function init() {
 }
 
 init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Create ajax calls to retrieve movie info
+
+/*var movieURL = "https://...";
+$.ajax({
+    url: URL,
+    method: "GET",
+}).then(function(response){
+
+    if (response.error === " NOT FOUND"){
+            //
+    }
+    else {
+    
+
+        //movie title
+        var mTitle = response.title;
+
+        //plot
+        var mplot = response.plot;
+
+
+        //rating
+        var mRating = response.rating;
+        
+
+
+        //runningTime
+        var mTime = response.runningTime;
+
+        //genre
+        var mgenre = response.genre;
+
+
+
+        //releaseDate
+        var mDate = response.releaseDate;
+
+
+
+    }
+
+}
+)
+
+
+
+//Create ajax calls to retrieve book info
+
+$.ajax({
+    url: bookURL,
+    method: "GET",
+  }).then(function (response1) {
+
+    if (response1.items === 0){
+
+
+
+    }
+
+
+
+
+    
+
+    else {
+
+
+
+
+    }
+
+
+  })
+*/
