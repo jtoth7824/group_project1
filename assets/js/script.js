6c267603b7551a90f8367d97404dd84c88543077
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2020-12-17 19:53:03 -0500

    queryParams and URL building

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,139 @@ var searchInfo = {
     },
     bookTitle: "Angels and Demons"
 };
+var title = "anne of green gables";
+var author = "Dan Brown";
+var isbn = "9780593123843";
+var authorid = "124249"; //Daniel Lieberman
+var authoreventisbn = "9781524746988";
+// var movideapiKey = "bea0d386";
+// var bookapiKey = "tfugk99hpk2nt8sm3ve3peqy";
+
+// The following url is how to retrieve the book cover.   Need an ISBN number
+// <img src="http://covers.openlibrary.org/b/isbn/9780385533225-S.jpg" />
 
 $("#searchBtn").on("click", function () {
 
 });
 
-function displaySearchParams() {
-
-}
-
 /* function to save author name and book title to local storage */
 function saveSearchInfo() {
 
     /* save book title and author name to local storage after changing object to String */
-    localStorage.setItem("SearchInfo", JSON.stringify(searchInfo)); 
+    localStorage.setItem("SearchInfo", JSON.stringify(searchInfo));
+}
+
+function convertSearchTerm(searchTerm) {
+    /* split book title into individual word array based upon space character as separator */
+    var convert = searchTerm.split(" ");
+    /* rejoin each array element with + between each word in string */
+    convert = convert.join("+");
+    return convert;
+}
+
+function buildMovieQueryURL() {
+    var queryURL = "http://www.omdbapi.com/?";
+
+    var movieTitle = convertSearchTerm(title);
+    var queryParams = {
+        "apikey": "bea0d386"
+    };
+    queryParams.t = movieTitle;
+    console.log(queryURL + $.param(queryParams));
+    return queryURL + $.param(queryParams);
+}
+
+function buildAuthorQueryURL() {
+    var queryURL = "https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/search/views/search-display?";
+
+    var queryParams = {
+        "api_key": "tfugk99hpk2nt8sm3ve3peqy"
+    };
+
+    queryParams.q = convertSearchTerm(author);
+    console.log(queryURL + $.param(queryParams));
+
+    return queryURL + $.param(queryParams);
+}
+
+function buildBookTitleQueryURL(isbn) {
+    var queryURL = "https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/titles/";
+
+    queryURL = queryURL + isbn + "?";
+    var queryParams = {
+        "api_key": "tfugk99hpk2nt8sm3ve3peqy"
+    };
+
+    console.log(queryURL + $.param(queryParams));
+
+    return queryURL + $.param(queryParams);
+}
+
+function buildEventQueryURL() {
+    var queryURL = "https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.US/authors/";
+
+    queryURL = queryURL + authorid + "/events?";
+
+    var queryParams = {
+        "api_key": "tfugk99hpk2nt8sm3ve3peqy"
+    };
+
+    queryParams.isbn = authoreventisbn;
+    queryParams.eventDateFrom = "12/15/2020";
+    queryParams.sort = "eventdate";
+    console.log(queryURL + $.param(queryParams));
+
+    return queryURL + $.param(queryParams);
+}
+
+function displayMovieInfo() {
+    var url;
+
+    /* build url for movie api call utilzing the book title chosen */
+    url = buildMovieQueryURL();
+    console.log(url);
+    /* make ajax call to retrieve movie object */
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (response) {
+            console.log(response);
+            /* clear movie info */
+
+            /* clear goes above here */
+
+            /* create elements to display movie info to page */
+
+            url1 = buildAuthorQueryURL();
+            $.ajax({
+                type: "GET",
+                url: url1,
+                success: function (response) {
+                    console.log(response);
+
+                    url2 = buildBookTitleQueryURL(isbn);
+                    $.ajax({
+                        type: "GET",
+                        url: url2,
+                        success: function (response) {
+                            console.log(response);
+
+                            url3 = buildEventQueryURL(isbn);
+                            $.ajax({
+                                type: "GET",
+                                url: url3,
+                                success: function (response) {
+                                    console.log(response);
+                                }
+                            })
+
+                        }
+                    })
+
+                }
+            })
+        }
+    })
 }
 
 function init() {
@@ -30,9 +149,10 @@ function init() {
     if (savedInfo === null) {
         localStorage.setItem("SearchInfo", JSON.stringify(searchInfo));
     }
+    displayMovieInfo();
 
     /* call to display the search params area */
-    displaySearchParams();
+//    displaySearchParams();
 }
 
 init();
